ec1f11465e368d899b3f17c9c335a9f81b6bbe8a
gurudhakshan <saravananraje01@gmail.com>
2024-08-02 12:36:01 +0530

    hi hello

---
+++ capegemini/src/Day0/AdditionalDemo.java
@@ -0,0 +1,18 @@
+package Day0;
+import java.util.*;
+public class AdditionalDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Addition");
+		int a=1;
+		int b=2;
+		int c= a+b;
+		System.out.println("the addition of the code is"+ c);
+		Scanner sc = new Scanner (System.in);
+		System.out.println("enter aa number");
+		int aa=sc.nextInt();
+		System.out.println("enter aa number");
+		int bb=sc.nextInt();
+		System.out.println("Addition is "+ aa+bb);		
+	}
+}
